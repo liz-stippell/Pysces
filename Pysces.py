2685f1cedccd0685cc1f5ea8fd3f4c6b503a7c75
liz-stippell <elstippe@buffalo.edu>
2020-05-22 22:06:05 -0400

    Help function now works
    Documentation was corrected.

--- Pysces.py
+++ Pysces.py
@@ -16,56 +16,79 @@ init_printing()
 
 
 def COMM_1(A, B, f):
+    """
+
+    This function is not used directly, it is only used in the COMM() function below. 
+
+    Parameters:
+
+    A: The first operator in the commutator
+    B: The second operator in the commutator
+    f: The auxiliary function. This is defined below, as F(x).
+
+    Returns:
+
+    The commutator of A and B with respect to F(x).
+
+    """
+    
     return expand((Commutator(Operator(A), Operator(B))*f).doit())
-"""
 
-This function is not used directly, it is only used in the COMM() function below. 
-
-Parameters:
-
-A: The first operator in the commutator
-B: The second operator in the commutator
-f: The auxiliary function. This is defined below, as F(x).
-
-Returns:
-
-The commutator of A and B with respect to F(x).
-
-"""
 
 
 
 def COMM(x, y, a, b = None):
-            if b == None:
-                return COMM_1(Operator(x), Operator(y), a)
-            elif b == 0:
-                return Commutator(Operator(x), Operator(a)) + Commutator(Operator(y), Operator(a))
-            else:       
-                return Commutator(Operator(x), Operator(a)) - Commutator(Operator(x), Operator(b)) + Commutator(Operator(y), Operator(a)) + Commutator(Operator(y), Operator(b))
-"""
+    """
 
-This function has a few different outputs, depending on the parameters.
+    This function has a few different outputs, depending on the parameters.
 
-Parameters:
-x: the first term of the first operator
-y: the second term of the firts operator
-a: the first term of the second operator
-b: the second term of the second operator
+    Parameters:
 
-The commutator looks similar to this: [x + y, a + b]
-An example would be the angular momentum operators, [L_x, L_y], or any similar combination.
+    x: the first term of the first operator
+    y: the second term of the firts operator
+    a: the first term of the second operator
+    b: the second term of the second operator
 
-Returns:
+    The commutator looks similar to this: [x + y, a + b]
+    An example would be the angular momentum operators, [L_x, L_y], or any similar combination.
 
-If b == None (this means there is no b parameter listed), the output will be COMM_1(), as defined above.
-If b == 0 (0 would be written in the argument), the output will be [x, a] + [y, a], such as used in the case of [L_x, x]
-Else, [x, a] - [x, b] + [y, a] + [y, b] would be returned, such as the case of [L_x, L_y]
+    Returns:
 
-"""
+    If b == None (this means there is no b parameter listed), the output will be COMM_1(), as defined above.
+    If b == 0 (0 would be written in the argument), the output will be [x, a] + [y, a], such as used in the case of [L_x, x]
+    Else, [x, a] - [x, b] + [y, a] + [y, b] would be returned, such as the case of [L_x, L_y]
+
+    """
+    
+    if b == None:
+        return COMM_1(Operator(x), Operator(y), a)
+    elif b == 0:
+        return Commutator(Operator(x), Operator(a)) + Commutator(Operator(y), Operator(a))
+    else:       
+        return Commutator(Operator(x), Operator(a)) - Commutator(Operator(x), Operator(b)) + Commutator(Operator(y), Operator(a)) + Commutator(Operator(y), Operator(b))
+
 
 
 
 def FACTOR_COMM(A, B, C, D):
+    """
+
+    Parameters:
+
+    A: The first term of the first operator
+    B: The second term of the first operator
+    C: The first term of the second operator
+    D: The second term of the second operator
+
+    Returns:
+
+    This function factors out terms based on the criteria set by the "if" statements. If a term in the first operator matches a term in the second operator, the commutator will be reported as zero ("0").
+    The criterion set examines the variables of each operator, and factors out operators that have "mismatched" variables (such as p with respect to z and y), while keeping the terms that are with respect to the same variable within a commutator, such as [p_x, x].
+
+    Note that if there are multiplte commutators, the FACTOR_COMM() functions can be added or subtracted ("+" or "-") in a single line for ease.
+
+    """
+    
     x, y, z, p_x, p_y, p_z = symbols("x y z p_x p_y p_z")
     if A == p_y and C == y:
         return (Operator(B)*Operator(D)*Commutator(Operator(A), Operator(C)) )
@@ -127,217 +150,232 @@ def FACTOR_COMM(A, B, C, D):
          return 0
     if B == D:
         return 0
-"""
 
-Parameters:
-
-A: The first term of the first operator
-B: The second term of the first operator
-C: The first term of the second operator
-D: The second term of the second operator
-
-Returns:
-
-This function factors out terms based on the criteria set by the "if" statements. If a term in the first operator matches a term in the second operator, the commutator will be reported as zero ("0").
-The criterion set examines the variables of each operator, and factors out operators that have "mismatched" variables (such as p with respect to z and y), while keeping the terms that are with respect to the same variable within a commutator, such as [p_x, x].
-
-Note that if there are multiplte commutators, the FACTOR_COMM() functions can be added or subtracted ("+" or "-") in a single line for ease.
-
-
-"""
 
 
 
 def P_OPERATOR(x):
+    """
+
+    Parameters:
+
+    x: what the linear momentum operator is with respect to.
+
+    Returns:
+
+    The linear momentum operator, with respect to the parameter.
+
+    Note that this equation is included in the Hamiltonian() function. 
+    Note that the "1" in the derivative is a placeholder, which will be manually replaced.
+
+    """
+    
     h_b, m = symbols("h_b m")
     return Operator(-I*h_b*(Derivative("1", x)))
-"""
 
-Parameters:
-
-x: what the linear momentum operator is with respect to.
-
-Returns:
-
-The linear momentum operator, with respect to the parameter.
-
-Note that this equation is included in the Hamiltonian() function. 
-Note that the "1" in the derivative is a placeholder, which will be manually replaced.
-
-"""
 
 
 
 def KINETIC_ENERGY(x):
+    """
+
+    Parameters:
+
+    x: The variable in which the derivative is with respect to.
+
+    Returns:
+
+    The kinetic energy operator, with respect to the chosen parameter. 
+
+    Note that (-(h_b)**2)*Derivative("1", x, x) == P_OP(x)**2
+    Note that the "1" in the derivative is a placeholder, which will be manually replaced.
+
+    """
+    
     h_b, m = symbols("h_b m")
     return (Operator((-I*h_b*(Derivative("1", x)))**2)*(1/(2*m)))
-"""
 
-This is subject to change.
-
-Parameters:
-
-x: The variable in which the derivative is with respect to.
-
-Returns:
-
-The Hamiltonian for a Particle in a Box, with respect to the chosen parameter. 
-
-Note that (-(h_b)**2)*Derivative("1", x, x) == P_OP(x)
-Note that the "1" in the derivative is a placeholder, which will be manually replaced.
-
-"""
 
 
 
 def EXPRESSION_REPLACE(R, K):
+    """
+
+    Parameters:
+
+    R: The expanded commutator to be replaced
+    K: The variable/parameter with respect to the chosen commutator.
+
+    Returns:
+
+    This replaces the Derivative(1, x) present in the expanded commutator with either Derivative(F(x), x) or Derivative(x*F(x), x).
+    Note that the above states "x", but can be done for x, y, or z variables.
+    Note that the R parameter is a str()
+
+
+    """
+    
     if K == x:
         return sympify(str(R).replace(str(Derivative(1, K)*F(K)), str(Derivative(F(K), K).doit())).replace(str('Derivative(1, x)*x*F(x)'), str(Derivative(K*F(K), K).doit())))
     if K == y:
         return sympify(str(R).replace(str(Derivative(1, K)*F(K)), str(Derivative(F(K), K).doit())).replace(str('Derivative(1, y)*y*F(y)'), str(Derivative(K*F(K), K).doit())))
     if K == z:
         return sympify(str(R).replace(str(Derivative(1, K)*F(K)), str(Derivative(F(K), K).doit())).replace(str('Derivative(1, z)*z*F(z)'), str(Derivative(K*F(K), K).doit())))
-"""
 
-Parameters:
-
-R: The expanded commutator to be replaced
-K: The variable/parameter with respect to the chosen commutator.
-
-Returns:
-
-This replaces the Derivative(1, x) present in the expanded commutator with either Derivative(F(x), x) or Derivative(x*F(x), x).
-Note that the above states "x", but can be done for x, y, or z variables.
-Note that the R parameter is a str()
-
-
-"""
 
 
 
 def F(x):
+    """
+
+    Parameters:
+
+    x: This is what the auxiliary function is with respect to. It should also match the parameters of the other arguments in the COMM() function. (example: if one operator is P_OP(y), the auxiliary function should be F(y).
+
+    Returns:
+
+    This is the auxiliary function, commonly used in the COMM() function.
+    This simply returns "F(x)", x being with chosen parameter.
+
+    """
+    
     return Operator(Function('F')(x))
-"""
 
-Parameters:
-
-x: This is what the auxiliary function is with respect to. It should also match the parameters of the other arguments in the COMM() function. (example: if one operator is P_OP(y), the auxiliary function should be F(y).
-
-Returns:
-
-This is the auxiliary function, commonly used in the COMM() function.
-This simply returns "F(x)", x being with chosen parameter.
-
-"""
 
 
 
 def HO():
+    """
+
+    Parameters:
+
+    Currently, there are no needed parameters for this equation.
+
+    Returns:
+
+    The Harmonic Oscillator Hamiltonian. Note that "p" is the linear momentum operator, and so the first term is the KINETIC_ENERGY() function mentioned above.
+
+    """
+    
     p, k, m, x = symbols("p k m x")
     return (p**2)/(2*m) + (1/2)*k*(x**2)
-"""
 
-Parameters:
-
-Currently, there are no needed parameters for this equation.
-
-Returns:
-
-The Harmonic Oscillator Hamiltonian. Note that "p" is the linear momentum operator, and so the first term is the Hamiltonian() mentioned above.
-
-"""
 
 
 
 def PLANEWAVE(x):
+    """
+
+    Parameters:
+
+    x: What the function is with respect to.
+
+    Returns:
+
+    The PlaneWave function with respect to the parameter.
+
+    """
+
     k, omega, t = symbols("k omega t")
     return exp(I*(k*x-omega*t))
-"""
-
-Parameters:
-
-x: What the function is with respect to.
-
-Returns:
-
-The PlaneWave function with respect to the parameter.
-
-"""
 
 
 
 def PIB(x, L, n):
+    """
+
+    Parameters:
+
+    x: a variable.
+    L: Length of the box.
+    n: an integer.
+
+    Returns:
+
+    The WaveFunction for Particle in a Box with respect to the chosen parameters.
+
+    """
+    
     return sin((n*pi*x)/L)
-"""
 
-Parameters:
-
-x: a variable.
-L: Length of the box.
-n: an integer.
-
-Returns:
-
-The WaveFunction for Particle in a Box with respect to the chosen parameters.
-
-"""
 
 
 
 def PIB_NORMALIZE(x, L, n):
+    """
+
+    Parameters:
+
+    x: a variable.
+    L: Length of the box.
+    n: an integer.
+
+    Returns:
+
+    The normalized WaveFunction for Particle in a Box, with respect to the chosen variables. This answer can also be calculated via the NORMALIZE() function.
+
+    """
+    
     return sqrt(2/L)*sin((n*pi*x)/L)
-"""
 
-Parameters:
-
-x: a variable.
-L: Length of the box.
-n: an integer.
-
-Returns:
-
-The normalized WaveFunction for Particle in a Box, with respect to the chosen variables. This answer can also be calculated via the NORMALIZE() function.
-
-"""
 
 
 
 def CONJUGATE(x):
+    """
+
+    Parameters:
+
+    x: The term of interest. This is commonly a WaveFunction.
+
+    Returns:
+
+    The complex conjugate of a WaveFunction. If there are imaginary terms, they get negated, and if there are no imaginary terms, the WaveFunction is not affected.
+
+    """
+    
     return x.replace(I, -I)
-"""
 
-Parameters:
-
-x: The term of interest. This is commonly a WaveFunction.
-
-Returns:
-
-The complex conjugate of a WaveFunction. If there are imaginary terms, they get negated, and if there are no imaginary terms, the WaveFunction is not affected.
-
-"""
 
 
 
 def NORMALIZE(A, x, y, z):
+    """
+
+    Parameters:
+
+    A: The WaveFunction/expression of interest
+    x: What the integral is taken with respect to
+    y: The lower bound of the integral. If bounds are not listed, this is -oo
+    z: The upper bound of the integral. If bounds are not listed, this is oo
+
+    Returns:
+
+    The normalization constant, with respect to the given parameters. To find the normalized WaveFunction, the output for NORMALIZE() must be multiplied by the original WaveFunction. A normalized WaveFunction indicates that the probability of finding a particle within certain bounds must be equal to one.
+
+    """
+    
     return 1/sqrt(Integral(A*CONJUGATE(A), (x, y, z)))
-"""
 
-Parameters:
-
-A: The WaveFunction/expression of interest
-x: What the integral is taken with respect to
-y: The lower bound of the integral. If bounds are not listed, this is -oo
-z: The upper bound of the integral. If bounds are not listed, this is oo
-
-Returns:
-
-The normalization constant, with respect to the given parameters. To find the normalized WaveFunction, the output for NORMALIZE() must be multiplied by the original WaveFunction. A normalized WaveFunction indicates that the probability of finding a particle within certain bounds must be equal to one.
-
-"""
 
 
 
 def EXPECTATION(A, B, x, y, z):
+    """
+
+    Parameters:
+
+    A: The normalized WaveFunction/expression of interest
+    B: The operator of interest.
+    x: What the integral is taken with respect to
+    y: The lower bound of the integral. If bounds are not listed, this is -oo
+    z: The upper bound of the integral. If bounds are not listed, this is oo
+
+    Returns:
+
+    The expectation value for the given operator and normalized WaveFunction. An expectation value is the average value of an operator for a given WaveFunction. 
+
+    """
     
     if B == KINETIC_ENERGY(x):
         return Integral(CONJUGATE(A)*B, (x, y, z)).replace(Derivative("1", x), Derivative(A, x).doit())
@@ -356,94 +394,88 @@ def EXPECTATION(A, B, x, y, z):
     
     else:
         return Integral(CONJUGATE(A)*B*A, (x, y, z))
-"""
 
-Parameters:
-
-A: The normalized WaveFunction/expression of interest
-B: The operator of interest.
-x: What the integral is taken with respect to
-y: The lower bound of the integral. If bounds are not listed, this is -oo
-z: The upper bound of the integral. If bounds are not listed, this is oo
-
-Returns:
-
-The expectation value for the given operator and normalized WaveFunction. An expectation value is the average value of an operator for a given WaveFunction. 
-
-"""
 
 
 
 def PLOT(A, B, C, D):
+    """
+
+    Parameters:
+
+    A: The function/Normalized WaveFunction of interest
+    B: This is "x" usually (x-axis)
+    C: The lower bound of the x-axis domain (for Particle in a Box, 0)
+    D: The upper bound of the x-axis domain (for Particle in a Box, 1)
+
+    Returns:
+
+    A plotted function of the function/Normalized WaveFunction of interest.
+
+    Note that the cell often has to be run TWICE in order to print the plot/graph.
+
+
+    """
+        
     return plot(sympify(str(A)), (B, C, D))
-"""
 
-Parameters:
-
-A: The function/Normalized WaveFunction of interest
-B: This is "x" usually (x-axis)
-C: The lower bound of the x-axis domain (for Particle in a Box, 0)
-D: The upper bound of the x-axis domain (for Particle in a Box, 1)
-
-Returns:
-
-A plotted function of the function/Normalized WaveFunction of interest.
-
-Note that the cell often has to be run TWICE in order to print the plot/graph.
-
-
-"""
 
 
 
 def LAGUERRE(r, n):
+    """
+
+    Parameters:
+
+    r: What the equation is with respect to. This is commonly "r" or "x"
+    n: The principle Quantum Number
+
+    Returns:
+
+    The Laguerre polynomial. This is commonly used to solve the LAGUERRE_ASSOC() function.
+
+    """
+    
     return simplify((exp(r)*Derivative(exp(-r)*(r**n), (r, n))).doit())
-"""
 
-Parameters:
-
-r: What the equation is with respect to. This is commonly "r" or "x"
-n: The principle Quantum Number
-
-Returns:
-
-The Laguerre polynomial. This is commonly used to solve the LAGUERRE_ASSOC() function.
-
-"""
 
 
 
 def LAGUERRE_2(r, n):
+    """
+
+    Parameters:
+
+    r: What the equation is with respect to. This is commonly "r" or "x"
+    n: The principle Quantum Number
+
+    Returns:
+
+    The Laguerre polynomial, without simplification and without the derivative computed.
+
+    """
+    
     return (exp(r)*Derivative(exp(-r)*(r**n), (r, n)))
-"""
 
-Parameters:
-
-r: What the equation is with respect to. This is commonly "r" or "x"
-n: The principle Quantum Number
-
-Returns:
-
-The Laguerre polynomial, without simplification and without the derivative computed.
-
-"""
 
 
 
 def LAGUERRE_ASSOC(n, l):
+    """
+
+    Parameters:
+
+    n: The principle Quantum Number
+    l: The angular momentum Quantum Number
+
+    Returns:
+
+    The Laguerre Associated polynomial, commonly used to solve the RADIAL() function.
+
+    """
+    
     return simplify(((-1)**l)*Derivative(LAGUERRE_2(r, n+l), (r, l)).doit())
-"""
 
-Parameters:
-
-n: The principle Quantum Number
-l: The angular momentum Quantum Number
-
-Returns:
-
-The Laguerre Associated polynomial, commonly used to solve the RADIAL() function.
-
-"""
 
 
 
